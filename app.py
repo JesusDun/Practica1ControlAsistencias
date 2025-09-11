577b6a2b32f4f191facb33b15b9972d87d9e32c5
RaulGuadalajara <omargdl0506@gmail.com>
2025-09-10 21:19:33 -0500

    Update app.py

--- app.py
+++ app.py
@@ -210,6 +210,7 @@ def departamentos():
 
 @app.route("/tbodyDepartamentos")
 def tbodyDepartamentos():
+    con = mysql.connector.connect(**db_config)
     try
         if not con.is_connected():
             con.reconnect()
