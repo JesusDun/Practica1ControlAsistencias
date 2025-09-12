71e00552aac4f4a669dc68a4d352a460de0cb9a1
JesusDun <alejandrovielmagarcia@gmail.com>
2025-09-12 14:25:18 -0500

    Update app.py

--- app.py
+++ app.py
@@ -40,49 +40,6 @@ def pusherAsistencias():
 app.secret_key = "pruebaLLaveSecreta_123"
 
 # =========================================================================
-# LOGIN
-# =========================================================================
-
-@app.route("/")
-def login():
-    if "idUsuario" in session:
-        return redirect(url_for("index"))
-    return render_template("login.html")
-
-@app.route("/iniciarSesion", methods=["POST"])
-def iniciarSesion():
-    usuario_ingresado = request.form["txtUsuario"]
-    contrasena_ingresada = request.form["txtContrasena"]
-
-    if not usuario_ingresado or not contrasena_ingresada:
-        return "Datos incompletos", 400
-
-    con = mysql.connector.connect(**db_config)
-    cursor = con.cursor(dictionary=True)
-    cursor.execute("SELECT idUsuario, username, password FROM usuarios WHERE username = %s", (usuario_ingresado,))
-    registro_usuario = cursor.fetchone()
-    con.close()
-
-    if registro_usuario:
-        hash_guardado = registro_usuario['password'].encode('utf-8')
-        contrasena_ingresada_bytes = contrasena_ingresada.encode('utf-8')
-        if bcrypt.checkpw(contrasena_ingresada_bytes, hash_guardado):
-            session["idUsuario"] = registro_usuario["idUsuario"]
-            session["username"] = registro_usuario["username"]
-            return redirect(url_for("index"))
-    return "Usuario o contraseña incorrectos", 401
-
-@app.route("/index")
-def index():
-    if "idUsuario" not in session:
-        return redirect(url_for("login"))
-    return render_template("index.html")
-
-@app.route("/cerrarSesion")
-def cerrarSesion():
-    session.clear()
-    return redirect(url_for("login"))
-# =========================================================================
 # MÓDULO EMPLEADOS
 # =========================================================================
 
